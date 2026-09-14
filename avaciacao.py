569d454c35792f3b514f484ab4315792b70acac4
kanudo <00001118706171sp@al.educacao.sp.gov.br>
2026-09-14 12:51:17 +0000

    ava

---
+++ avaciacao.py
@@ -0,0 +1,23 @@
+startup = {
+    "nome": "cyberPulse Tech",
+    "seguimento": "segurança da informação",
+    "ano_adesao":"2026"
+}
+
+solucoes_ativas = ["firewall IA", "Scan de vulnerabilidades"]
+
+print(startup["nome"],"-",
+startup["seguimento"])
+print(solucoes_ativas[0])
+
+bancadas = [
+    [1, 0],
+    [0, 1]
+]
+
+print(bancadas[0][0])
+print(bancadas[0][1])
+print(bancadas[1][0])
+print(bancadas[1][1])
+
+print("legenda: 1 = ocupado | 0 livre")
